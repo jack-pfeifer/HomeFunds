13bb32e75d57a9f20ed5f7f15202db4c810647bf
Jack Pfeifer <jpfeifer@albany.edu>
2026-04-15 14:42:37 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const path = require("path");
 const expenseRoutes = require("./routes/expenses");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +29,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/expenses", expenseRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
