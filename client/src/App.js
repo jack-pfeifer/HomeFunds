a90ab009c087777dea38502e8d91aac1b92e7f90
Jack Pfeifer <jpfeifer@albany.edu>
2026-04-15 15:34:38 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 const PAGES = {
   CONNECT: "CONNECT",
